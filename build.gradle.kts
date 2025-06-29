629b23137f32c56b5df8806947bcb25d8e7dac74
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-06-29 23:17:52 +0100

    Use Gradle Version Catalogs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.3"
-	id("io.spring.dependency-management") version "1.1.7"
-	id("org.asciidoctor.jvm.convert") version "4.0.4"
+	alias(libs.plugins.springBoot)
+	alias(libs.plugins.springDependencyManagement)
+	alias(libs.plugins.asciiDoctor)
 }
 
 group = "uk.co.emcreations"
