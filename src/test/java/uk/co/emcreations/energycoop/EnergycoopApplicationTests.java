a6a92a407869387b8160b2cdbf8bde4888711135
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-06-29 22:33:16 +0100

    Initial commit.

---
+++ src/test/java/uk/co/emcreations/energycoop/EnergycoopApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.emcreations.energycoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergycoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
