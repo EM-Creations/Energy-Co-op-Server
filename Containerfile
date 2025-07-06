44131e9d3ff043e3d24b6d58a5e25ebe9fafca42
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-07-06 20:42:30 +0100

    Scheduling task.

--- Containerfile
+++ Containerfile
@@ -1,4 +1,4 @@
 FROM azul/zulu-openjdk-debian:21.0.7
 MAINTAINER em-creations.co.uk
-COPY build/energycoop-*.jar application.jar
+COPY build/libs/energycoop-*.jar application.jar
 ENTRYPOINT ["java", "-jar", "/application.jar"]
---
+++ src/main/java/uk/co/emcreations/energycoop/config/SchedulerConfig.java
@@ -0,0 +1,25 @@
+package uk.co.emcreations.energycoop.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import uk.co.emcreations.energycoop.service.GraigFathaStatsService;
+
+@Slf4j
+@Configuration
+@EnableScheduling
+@RequiredArgsConstructor
+public class SchedulerConfig {
+    @Autowired
+    private final GraigFathaStatsService graigFathaStatsService;
+
+    @Scheduled(cron = "${scheduling.graig-fatha.schedule:*/15 * * * *}")
+    public void logEnergyYield() {
+        log.info("logEnergyYield running..");
+
+        log.info("Response = {}", graigFathaStatsService.getEnergyYield());
+    }
+}
