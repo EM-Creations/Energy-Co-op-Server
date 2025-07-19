d143caa94d774fbd0e126a0712572c0c70e4f91e
Edward McKnight <8123733+EM-Creations@users.noreply.github.com>
2025-07-19 21:11:37 +0100

    #4: Fix ManagementController tests.

--- src/test/java/uk/co/emcreations/energycoop/controller/ManagementControllerTest.java
+++ src/test/java/uk/co/emcreations/energycoop/controller/ManagementControllerTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.co.emcreations.energycoop.service.AuthService;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,6 +17,9 @@ class ManagementControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private AuthService authService;
 
     @Test
     @DisplayName("GET /hello returns 200 OK")
